8a4a5d18a98aed7b8eb5b4d8fe8b41cf80ebea65
rajagopalan-cadet <rajagopalan@cadetprogram.org>
2024-08-26 11:46:33 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+ const accessToken = '00DC1000000P5Nt!AQEAQNUUA.dEjoN9ZqW4pvLVB45E.TN_6YEwdJOryaZqQXRowYL.FhnHPKkJmHtCUj9MY173jJD0.wd9YbRCn8bkIOW.G7WA'; // Replace with your actual access token
+ const instanceUrl = 'https://cadetprogram--charcoal.sandbox.my.salesforce.com'; // Replace with your actual instance URL
