58df9bef2315244ea8e2a04308ed59449f5d41a8
rajagopalan-cadet <rajagopalan@cadetprogram.org>
2024-08-26 12:03:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,9 +15,7 @@ document.getElementById("userForm").addEventListener("submit", async function(ev
     document.getElementById("userForm").reset();
 
     // Now create the Salesforce record
-    const accessToken = '00DC1000000P5Nt!AQEAQNUUA.dEjoN9ZqW4pvLVB45E.TN_6YEwdJOryaZqQXRowYL.FhnHPKkJmHtCUj9MY173jJD0.wd9YbRCn8bkIOW.G7WA'; // Replace with your actual access token
-    const instanceUrl = 'https://cadetprogram--charcoal.sandbox.my.salesforce.com'; // Replace with your actual instance URL
-
+   
     const url = `${instanceUrl}/services/data/v52.0/sobjects/Contact/`; // API endpoint for Contact
     const recordData = {
         FirstName: firstname,
