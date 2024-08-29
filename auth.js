ba40228a9f6076e85bbc50093b362b337e174992
rajagopalan-cadet <rajagopalan@cadetprogram.org>
2024-08-30 00:11:10 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { getAuth, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
