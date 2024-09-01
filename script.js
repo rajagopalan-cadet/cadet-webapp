6753f1cd8c4d9fbe4b15cbd8caf4bd14a7fa91a9
rajagopalan-cadet <rajagopalan@cadetprogram.org>
2024-09-01 15:35:00 +0530

    Update script.js
    Added loadGlobalComponents

--- script.js
+++ script.js
@@ -11,4 +11,21 @@ document.addEventListener('DOMContentLoaded', function() {
   loadHTML('footer', 'footer.html');
 });
 
+// Function to load common HTML content
+function loadGlobalComponents() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'common.html', true);
+    xhr.onload = function() {
+        if (this.status >= 200 && this.status < 400) {
+            document.body.insertAdjacentHTML('beforeend', this.responseText);
+        } else {
+            console.error('Failed to load global components.');
+        }
+    };
+    xhr.send();
+}
+
+// Load common content on page load
+window.addEventListener('DOMContentLoaded', loadGlobalComponents);
+
 
