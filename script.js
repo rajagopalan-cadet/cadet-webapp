0962069771109d9ba060550cb48bf0f6f3527a25
rajagopalan-cadet <rajagopalan@cadetprogram.org>
2024-09-01 14:56:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,99 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyADwHvTveDow6kx2cIjKnNuG7gYlsfzNWk",
-  authDomain: "my-cadet-web-app.firebaseapp.com",
-  projectId: "my-cadet-web-app",
-  storageBucket: "my-cadet-web-app.appspot.com",
-  messagingSenderId: "1094130867052",
-  appId: "1:1094130867052:web:54157e132f5c7e64f1b149",
-  measurementId: "G-8SJV2FFEJ8"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-const provider = new GoogleAuthProvider();
-
-
-// Global variables to store trainer information
-let trainerId = null;
-let trainerRecordId = null;
-
-// Function to check Salesforce records
-async function checkSalesforceRecord(email) {
-    const instanceUrl = "https://cadetprogram--charcoal.sandbox.my.salesforce.com"; // Replace with your Salesforce instance URL
-    const queryUrl = `${instanceUrl}/services/data/v52.0/sobjects/Contact/CADET_Official_Email__c/${email}`;
-    
-    try {
-        const response = await fetch(queryUrl, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer 00DC1000000P5Nt!AQEAQDdRv.lfFGamJrAzgYZEfMUMZDF87l0NOvKnKSlqeT2It2_AjCG58VlW1qrmWTjDMse.rJsNgXffGTuuUBHAZkX5X__P`, // Replace with your Salesforce access token
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (response.status === 404) {
-           // Handle the case where no records are found
-            throw new Error('Error Authenticating Certified Trainer: No records found');
-        }
-      if (response.status === 300) {
-            // Handle the case where multiple records are found
-            throw new Error('Contact Admin. Multiple trainers found with this email.');
-        }
-      if (!response.ok) {
-            throw new Error('Error fetching Salesforce record');
-        }
-
-        const data = await response.json();
-      
-        if (data.Certification_Status__c === 'Certified') {
-            trainerId = data.CADET_Trainer_ID__c;
-            trainerSalesforceId = data.Id;
-            return true;
-        } else {
-           throw new Error('Error Authenticating Certified Trainer: User is not certified');
-        }
-    } catch (error) {
-       console.error(error.message);
-        // Log out the user if the check fails
-        await signOut(auth);
-        alert(error.message);
-        return false;
-    }
+function loadHTML(id, url) {
+  fetch(url)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(id).innerHTML = data;
+    });
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+  loadHTML('header', 'header.html');
+  loadHTML('footer', 'footer.html');
+});
 
 
-const userSignIn = async () => {
-    try {
-        const result = await signInWithPopup(auth, provider);
-        const user = result.user;
-
-       // Check Salesforce record for the authenticated user
-        const isCertified = await checkSalesforceRecord(user.email);
-
-        if (isCertified) {
-            window.location.href = "https://app.cadetprogram.org/home";
-        } 
-    } catch (error) {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.error(`Error code: ${errorCode}, message: ${errorMessage}`);
-    }
-}
-
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        window.location.href = "https://app.cadetprogram.org/home"
-    } else {
-        signInButton.style.display = "block";
-    }
-})
-
-const signInButton = document.getElementById("signInButton");
-signInButton.addEventListener('click', userSignIn);
